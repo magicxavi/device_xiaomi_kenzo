958d90ef0d4056a0a176d40121447ffba8de96e7
BH36B <67626089+ikeh045@users.noreply.github.com>
2020-12-12 21:20:56 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_kenzo.mk
+    $(LOCAL_DIR)/rr_kenzo.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_kenzo-userdebug \
-    havoc_kenzo-eng
+    rr_kenzo-userdebug \
+    rr_kenzo-eng
